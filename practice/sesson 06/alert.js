550a6653229469eeb3f7fa2e7a3a4370a7253c83
nguyenthanhtung32 <binbungbu6201@gmail.com>
2022-12-09 21:20:03 +0700

    thuchanhss06

---
+++ practice/sesson 06/alert.js
@@ -0,0 +1,34 @@
+let num = 1;
+console.log("number", num);
+let str = "Đức Đù";
+console.log("string", str);
+let ar = [1, 2, 3, 4];
+console.log("array", ar[0]);
+console.log("array", ar[1]);
+console.log("array", ar[2]);
+console.log("array", ar[3]);
+const srt = `Chào mừng các bạn đã đến với Channel của mình ${num}`;
+console.log(srt);
+console.log(srt);
+console.log(srt);
+console.log(srt);
+function getString() {
+  //console.log("getAgeUser");
+  return "Đức yêu Châu Khánh Linh";
+}
+let res = getString();
+console.log("function", res);
+let object = {
+  UserName: "tun_2306",
+  UserAge: 21,
+};
+console.log("object", object);
+console.log("object.name", object.UserName);
+console.log("object.age", object.UserAge);
+let boo = true;
+console.log("boolean", boo);
+let und;
+let unde = undefined;
+console.log("underfined", und, unde);
+let nu = null;
+console.log("null", nu);
